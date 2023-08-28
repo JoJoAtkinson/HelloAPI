6919c62a54ea11083703b409cf62110495fa62fa
Joseph Atkinson <atjoseph@live.com>
2023-08-28 10:35:08 -0400

    added writing to db example

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -1,11 +1,16 @@
 
-
-import json
+import boto3, json
+from boto3_type_annotations.dynamodb.service_resource import ServiceResource
 
 def lambda_handler(event, context):
     """ Handles function event and context """
 
     # https://dev.to/vahdet/creating-a-ci-cd-pipeline-for-a-python3-lambda-function-through-aws-cloudformation-4ck6
+
+    db: ServiceResource = boto3.resource("dynamodb", region_name="us-east-2")
+    
+    tbl = db.Table("demo_table")
+    tbl.put_item(Item={"id": "hello", "value": "world"})
 
     # TODO implement further business logic
     return {
